d13f9969a6050acfb56be6abb9812feb67ef91ee
Vitalij Nikonov <BarsStudio@gmail.com>
2025-03-29 15:24:40 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ const { showAlert } = usePopup();
   </header>
 
   <main>
-    <script async src="https://js.onclckmn.com/static/onclicka.js" data-admpid="310907"></script>
+    
   </main>
 </template>
 
